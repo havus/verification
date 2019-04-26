894a2148cbc2007a4055fe3a3053dd033fbb78bd
root <root@localhost.localdomain>
2019-04-27 06:31:35 +0700

    mq 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,23 +12,23 @@
 // x.addListener(mediaQuery);
 
 // media query event handler
-if (matchMedia) {
-    const mq = window.matchMedia("(min-width: 361px)");
-    mq.addListener(WidthChange);
-    WidthChange(mq);
-}
+// if (matchMedia) {
+//     const mq = window.matchMedia("(min-width: 361px)");
+//     mq.addListener(WidthChange);
+//     WidthChange(mq);
+// }
 
-// media query change
-function WidthChange(mq) {
-    if (mq.matches) {
-        // window width is at least 500px
-        // console.log("diatas 500");
-        let x = document.getElementById('main');
-        x.className = "main col-sm-4 offset-4";
-    } else {
-        // window width is less than 500px
-        let x = document.getElementById('main');
-        // console.log("dibawah 500");
-        x.className = "main col-sm-4";
-    }    
-}
+// // media query change
+// function WidthChange(mq) {
+//     if (mq.matches) {
+//         // window width is at least 500px
+//         // console.log("diatas 500");
+//         let x = document.getElementById('main');
+//         x.className = "main col-sm-4 offset-4";
+//     } else {
+//         // window width is less than 500px
+//         let x = document.getElementById('main');
+//         // console.log("dibawah 500");
+//         x.className = "main col-sm-4";
+//     }    
+// }
